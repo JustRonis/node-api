a6fa36921eae5698cb8718262d86104c41f9816d
Ronis <ronaldo.luizcp@gmail.com>
2021-04-22 02:16:08 -0300

    Ajustando os status code

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -2,16 +2,16 @@ const moment = require('moment')
 const conexao = require('../infra/conexao')
 
 class Atendimento{
-    adiciona(atendimento){
+    adiciona(atendimento, res){
         const dataCriacao = moment().format('YYYY-MM-DD HH:MM>SS')
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM>SS')
         const atendimentoDatado = {...atendimento, dataCriacao, data};
         const sql = 'INSERT INTO Atendimentos SET ?'
         conexao.query(sql, atendimentoDatado,(erro, resultados) => {
             if(erro){
-                console.log(erro)
+                res.status(400).json(erro)
             }else{
-                console.log(resultados);
+                res.status(201).json(resultados);
             }
         })
     }
